71b6be543a0094ba169037aa660de2d23c1e96db
goneil <goneil@google.com>
2017-07-05 13:59:57 -0700

    Formatted parens.  Made logs easier to read

--- flask_server.py
+++ flask_server.py
@@ -5,21 +5,23 @@ import urllib
 from flask import Flask, request, jsonify
 
 config = {
-    'base_path': os.sep.join((expanduser("~"), 'Music', 'Pandora'))
+    'base_path': os.sep.join( ( expanduser( "~" ), 'Music', 'Pandora' ) )
 }
 
 app = Flask( __name__ )
 
 
 @app.route( '/download', methods=['POST'] )
-def pandoraDownloader():
+def pandoraDownloader( ):
     songTitle = request.form['title']
     songArtist = request.form['artist']
     songAlbum = request.form['album']
 
-    print ("Song title: " + songTitle)
-    print ("Song artist: " + songArtist)
-    print ("Song album: " + songAlbum)
+    print ( "---------------------------------" )
+    print ( "Received Download Request for ..." )
+    print ( songTitle)
+    print ( songArtist )
+    print ( songAlbum )
 
     song_path = make_song_path( songTitle, songArtist )
     song_dir  = make_song_path( songTitle, songArtist, False )
@@ -33,14 +35,15 @@ def pandoraDownloader():
         print( 'Downloading song!' )
         if not os.path.exists( song_dir ): os.makedirs( song_dir )
         urllib.urlretrieve( request.form['url'], song_path )
+        # os.system("./setArtists.sh")
         return jsonify( status = 'success' )
 
 
 def make_song_path( title, artist, with_file = True ):
     if with_file:
-        return os.sep.join( (config['base_path'], artist, title) ) + '.mp4'
+        return os.sep.join( ( config['base_path'], artist, title ) ) + '.mp4'
 
-    return os.sep.join( (config['base_path'], artist) )
+    return os.sep.join( ( config['base_path'], artist ) )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run( debug=True )
